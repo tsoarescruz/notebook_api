6c0b8342c327b5fb6b21259bc6439da2866552b6
Thiago Cruz <tcruz@horizonte.tv.br>
2019-01-05 16:25:04 -0200

    feat inser relationship with kind in contacts model, and override as_json method to make one principal

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
-end
+  belongs_to :kind
+
+  def author
+    "Jackson Pires"
+  end
+
+  #Metodo que faz o overide para o render json
+  #Forma mais facil de deixar padronizado o retorno do json para um objeto
+
+  def as_json(options={})
+    super(methods: :author, root:true)
+  end
+send
