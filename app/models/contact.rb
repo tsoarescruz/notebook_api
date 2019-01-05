c3704607702612e05ed6029c45f47794755ce36c
Thiago Cruz <tcruz@horizonte.tv.br>
2019-01-05 16:58:16 -0200

    feat cont methods in models to follow class notebook

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,30 @@
 class Contact < ApplicationRecord
   belongs_to :kind
 
-  def author
-    "Jackson Pires"
-  end
-
-  #Metodo que faz o overide para o render json
-  #Forma mais facil de deixar padronizado o retorno do json para um objeto
-
-  def as_json(options={})
-    super(methods: :author, root:true)
-  end
-send
+  # def author
+  #   "Jackson Pires"
+  # end
+  #
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # #Metodo que faz o overide para o render json
+  # #Forma mais facil de deixar padronizado o retorno do json para um objeto
+  #
+  # def as_json(options={})
+  #   super(
+  #     # Esse tipo de chamada de metodo para o kind_description e complementar ao
+  #     #include, se nao quiser que ele apareca como nested do kind e sim common
+  #     #um atributo para o no principal
+  #
+  #     # methods: [:author, :kind_description]
+  #     methods: [:author],
+  #     root:true,
+  #     #Utilizando o only para trazer somente os atributes que se quer
+  #     # include: { kind: { only: :description }}
+  #     include: :kind
+  #   )
+  # end
+end
